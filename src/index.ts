ca23cb7e7e46f79cd714987b337174fac0b49a5b
Benjamin Zalasky <benjamin.zalasky@lookout.com>
2022-09-26 13:20:03 -0500

    Update code transformer for Jest 28+

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import * as crypto from "crypto";
 import * as yaml from "js-yaml";
-import type { Config, Path, TransformOptions, Transformer } from "./jest-types";
+import type {
+  Config,
+  Path,
+  TransformOptions,
+  TransformedSource,
+  Transformer,
+} from "./jest-types";
 
 const getCacheKey = (
   fileData: string,
@@ -24,10 +30,15 @@ export const process = (
   sourcePath: Path,
   config: Config,
   options?: TransformOptions
-): string => {
+): TransformedSource => {
   const result = yaml.load(sourceText);
   const json = JSON.stringify(result, undefined, "\t");
-  return `module.exports = ${json}`;
+
+  // process() and processAsync() methods of a custom transformer module cannot
+  // return a string anymore. They must always return an object.
+  //
+  // https://jestjs.io/docs/28.x/upgrading-to-jest28#transformer
+  return { code: `module.exports = ${json}` };
 };
 
 const transformer: Transformer = {
